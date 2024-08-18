7f8da0e07740a7685ade76f92b53fc65a353835c
Makfax911 <maxbahniuk@gmail.com>
2024-08-18 18:30:34 +0300

    first commit

---
+++ DocumentManager.java
@@ -0,0 +1,110 @@
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.*;
+
+/**
+ * For implement this task focus on clear code, and make this solution as simple readable as possible
+ * Don't worry about performance, concurrency, etc
+ * You can use in Memory collection for sore data
+ * <p>
+ * Please, don't change class name, and signature for methods save, search, findById
+ * Implementations should be in a single class
+ * This class could be auto tested
+ */
+public class DocumentManager {
+
+    /**
+     * Implementation of this method should upsert the document to your storage
+     * And generate unique id if it does not exist, don't change [created] field
+     *
+     * @param document - document content and author data
+     * @return saved document
+     */
+    HashMap<String, Document> documents = new HashMap<>();
+    public Document save(Document document) {
+        if (document.getId() == null || document.getId().isEmpty()) {
+            String uniqueId = UUID.randomUUID().toString();
+            document.setId(uniqueId);
+        }
+        documents.put(document.getId(), document);
+        return document;
+    }
+
+    /**
+     * Implementation this method should find documents which match with request
+     *
+     * @param request - search request, each field could be null
+     * @return list matched documents
+     */
+
+    public List<Document> search(SearchRequest request) {
+
+        return documents.values().stream()
+                .filter(doc -> matchesTitlePrefixes(doc, request.getTitlePrefixes()))
+                .filter(doc -> matchesContents(doc, request.getContainsContents()))
+                .filter(doc -> matchesAuthorIds(doc, request.getAuthorIds()))
+                .filter(doc -> matchesCreatedFrom(doc, request.getCreatedFrom()))
+                .filter(doc -> matchesCreatedTo(doc, request.getCreatedTo()))
+                .toList();
+    }
+    private boolean matchesTitlePrefixes(Document document, List<String> titlePrefixes) {
+        return titlePrefixes == null || titlePrefixes.stream().anyMatch(prefix -> document.getTitle().startsWith(prefix));
+    }
+
+    private boolean matchesContents(Document document, List<String> containsContents) {
+        return containsContents == null || containsContents.stream().anyMatch(content -> document.getContent().contains(content));
+    }
+
+    private boolean matchesAuthorIds(Document document, List<String> authorIds) {
+        return authorIds == null || authorIds.contains(document.getAuthor().getId());
+    }
+
+    private boolean matchesCreatedFrom(Document document, Instant createdFrom) {
+        return createdFrom == null || !createdFrom.isAfter(document.getCreated());
+    }
+
+    private boolean matchesCreatedTo(Document document, Instant createdTo) {
+        return createdTo == null || !createdTo.isBefore(document.getCreated());
+    }
+
+    /**
+     * Implementation this method should find document by id
+     *
+     * @param id - document id
+     * @return optional document
+     */
+    public Optional<Document> findById(String id) {
+
+        return Optional.ofNullable(documents.get(id));
+    }
+
+    @Data
+    @Builder
+    public static class SearchRequest {
+        private List<String> titlePrefixes;
+        private List<String> containsContents;
+        private List<String> authorIds;
+        private Instant createdFrom;
+        private Instant createdTo;
+    }
+
+    @Data
+    @Builder
+    public static class Document {
+        private String id;
+        private String title;
+        private String content;
+        private Author author;
+        private Instant created;
+
+    }
+
+    @Data
+    @Builder
+    public static class Author {
+        private String id;
+        private String name;
+    }
+}
